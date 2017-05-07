45a04043d052b1e0866a349f9ca11168d833224d
Uriel Duran <uduran12@gmail.com>
2017-05-06 21:36:45 -0500

    ANOTHER ONE

--- Motivation.cmd
+++ Motivation.cmd
@@ -3,7 +3,7 @@
 
 
 ::set /a var=%random% %%22 +1
-set var=22
+set var=18
 echo %var%
 
 ECHO ______________________________________________________________________________________________
@@ -30,16 +30,165 @@ IF /I "%var%" EQU "20" ECHO The other day the grass was brown, its green cuz i a
 IF /I "%var%" EQU "21" ECHO Give thanks to the most high.
 IF /I "%var%" EQU "22" ECHO They dont want you to win. They dont want you to have the No. 1 record in the country. && echo They dont want you get healthy. They dont want you to exercise. && echo And they dont want you to have that view.
 IF /I "%var%" EQU "23" ECHO We go hard. In everything we do we’re going to accomplish our victory and our goal. If it takes a day, a year, or 20 years, we’re going to win.
-IF /I "%var%" EQU "24"
-IF /I "%var%" EQU "25"
-IF /I "%var%" EQU "26"
-IF /I "%var%" EQU "27"
-IF /I "%var%" EQU "28"
-IF /I "%var%" EQU "29"
-IF /I "%var%" EQU "30"
-IF /I "%var%" EQU "31"
-IF /I "%var%" EQU "32"
-IF /I "%var%" EQU "33"
+IF /I "%var%" EQU "24" ECHO Bless up.
+IF /I "%var%" EQU "25" ECHO Always have Faith. Always have Hope
+IF /I "%var%" EQU "26" ECHO I know that I’ve been put on this Earth to make people happy, to inspire people, and to uplift people. That’s a beautiful thing.
+IF /I "%var%" EQU "27" ECHO Be a star. Be a Superstar.
+IF /I "%var%" EQU "28" ECHO I remember when I ain’t have a jacuzzi.
+IF /I "%var%" EQU "29" ECHO Almond milk + cinnamon crunch = major key to success.
+IF /I "%var%" EQU "30" ECHO When you stop making excuses and you work hard and go hard you will be very successful.
+IF /I "%var%" EQU "31" ECHO The key is to enjoy life, because they don’t want you to enjoy life.
+IF /I "%var%" EQU "32" ECHO In life everyone has a choice. The key is: make a right choice.
+IF /I "%var%" EQU "33" ECHO Bless up. Don’t play yourself.
+IF /I "%var%" EQU "34" ECHO You do know it cost money to put a t-shirt on your back? You do know it cost money have a house? You do know it cost money to eat? Get money, don’t let these people fool you.
+IF /I "%var%" EQU "35" ECHO We have to get money. We have no choice. It cost money to eat.
+IF /I "%var%" EQU "36" ECHO They never said winning was easy.
+IF /I "%var%" EQU "37" ECHO Working all winter shining all summer.
+IF /I "%var%" EQU "38" ECHO Give thanks to the most high.
+IF /I "%var%" EQU "39" ECHO Some of the guys when they play, they try to keep it reality. Nah, I need the best everything.
+IF /I "%var%" EQU "40" ECHO You are here:
+Home  Latest  entertainment Dj Khaled Quotes: The Keys To Success
+ENTERTAINMENT, MUSIC,
+Dj Khaled Quotes: The Keys To Success
+
+by  Quotezine Team
+about a year ago
+59.7k Views
+
+13
+SHARES
+Share
+Tweet
+Pin
+@djkhaled has quickly become the hip hop Tony Robbins with his quotable keys to success series. If you follow him on Snapchat and/or Instagram, you’ve probably come across a few of these.
+
+As a DJ, Khaled has always been known for his famous one-liners and lyrics on songs, but this social media series of ad-libs is winning him a lot of new fans.
+
+Below are many of the most memorable “keys” and other classic Dj Khaled quotes.
+
+Another One!
+
+
+
+They don’t want you to win – Dj Khaled
+
+
+ dj-khaled-quote-key-visionary-2
+
+
+
+Watch your back, but more importantly when you get out the shower, dry your back. It’s a cold world out there. – DJ Khaled
+
+
+
+Be a star. Be a Superstar. – DJ Khaled
+
+
+
+I remember when I ain’t have a jacuzzi – DJ Khaled
+
+dj-khaled--quote-4
+
+All I do is WIN, WIN, WIN no matter what – DJ Khaled
+
+
+
+The other day the grass was brown, now its green cuz I ain’t give up. Never surrender. – DJ Khaled
+
+
+
+Almond milk + cinnamon crunch = major key to success. – DJ Khaled
+
+dj-khaled-quote-key-visionary-opt
+
+
+
+When you stop making excuses and you work hard and go hard you will be very successful. – DJ Khaled
+
+
+
+The key is to enjoy life, because they don’t want you to enjoy life. – DJ Khaled
+
+
+
+They’ll try to close the door on you… Just open it. – DJ Khaled
+
+dj-khaled--quote-5
+
+To succeed, you must believe. When you believe, you will succeed. – DJ Khaled
+
+
+
+In life everyone has a choice. The key is: make a right choice. – DJ Khaled
+
+
+
+Bless up. Don’t play yourself. – DJ Khaled
+
+
+
+download
+
+The key is to be honest. Be honest, but don’t play yourself. – DJ Khaled
+
+
+
+You do know it cost money to put a t-shirt on your back? You do know it cost money have a house? You do know it cost money to eat? Get money, don’t let these people fool you. – DJ Khaled
+
+
+
+We have to get money. We have no choice. It cost money to eat. – DJ Khaled
+
+
+
+The key to more success is coco butter. – DJ Khaled
+
+
+
+They never said winning was easy. – DJ Khaled
+
+
+
+dj-khaled-instagram-opt
+
+I know that I’ve been put on this Earth to make people happy, to inspire people, and to uplift people. That’s a beautiful thing. – DJ Khaled
+
+
+
+We go hard. In everything we do we’re going to accomplish our victory and our goal. If it takes a day, a year, or 20 years, we’re going to win. I haven’t taken a loss because everything I’ve done has been a working process to win. From being a kid on them turntables to becoming where I am is not a loss. It’s a blessing. – DJ Khaled
+
+
+
+I can deal with everything. I got the answer for anything. – DJ Khaled
+
+
+
+There will be roadblocks but we will overcome them. – DJ Khaled
+
+
+
+Key to more success is a clean heart and a clean face. – DJ Khaled
+
+
+
+Working all winter shining all summer – DJ Khaled
+
+
+
+Give thanks to the most high. – DJ Khaled
+
+Celebrity-fashion-Instagram-DJ-Khaled-opt
+
+Some of the guys when they play, they try to keep it reality. Nah, I need the best everything. – DJ Khaled
+
+
+
+Always have Faith. Always have Hope. – DJ Khaled
+
+
+
+The key is: never fold.
+
 
 
 ECHO.
