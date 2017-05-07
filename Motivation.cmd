c96fceb0aedd4016694cab99cb33143eb7fcb11a
Uriel Duran <uduran12@gmail.com>
2017-05-06 21:00:20 -0500

    If statements

--- Motivation.cmd
+++ Motivation.cmd
@@ -1,6 +1,16 @@
 :: Turns off commands from printing to screen.
 @ECHO OFF
 
-SET hello=Hellow World test test
-echo %hello%
+set /a var=%random% %%10 +1
+Echo %var%
+
+IF /I "%var%" EQU "1" ECHO
+IF /I "%var%" EQU "2" ECHO
+IF /I "%var%" EQU "3" ECHO
+IF /I "%var%" EQU "4" ECHO
+IF /I "%var%" EQU "5" ECHO
+IF /I "%var%" EQU "6" ECHO
+IF /I "%var%" EQU "7" ECHO
+IF /I "%var%" EQU "8" ECHO  
+
 pause
