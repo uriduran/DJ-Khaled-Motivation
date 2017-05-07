c4dcb43a76f51f8e871b0cec3ef485575874b41b
Uriel Duran <uduran12@gmail.com>
2017-05-06 21:27:50 -0500

    Adding Quotes

--- Motivation.cmd
+++ Motivation.cmd
@@ -1,16 +1,55 @@
 :: Turns off commands from printing to screen.
 @ECHO OFF
 
-set /a var=%random% %%10 +1
-Echo %var%
 
-IF /I "%var%" EQU "1" ECHO
-IF /I "%var%" EQU "2" ECHO
-IF /I "%var%" EQU "3" ECHO
-IF /I "%var%" EQU "4" ECHO
-IF /I "%var%" EQU "5" ECHO
-IF /I "%var%" EQU "6" ECHO
-IF /I "%var%" EQU "7" ECHO
-IF /I "%var%" EQU "8" ECHO  
+::set /a var=%random% %%22 +1
+set var=22
+echo %var%
+
+ECHO ______________________________________________________________________________________________
+ECHO.
+IF /I "%var%" EQU "1" ECHO Another one, no. Another two, drop two singles at a time.
+IF /I "%var%" EQU "2" ECHO Congratulations, you played yourself.
+IF /I "%var%" EQU "3" ECHO I can deal with everything. I got the answer for anything. This DJ Khaled.
+IF /I "%var%" EQU "4" ECHO The key is to make it.
+IF /I "%var%" EQU "5" ECHO There will be road blocks but we will overcome it.
+IF /I "%var%" EQU "6" ECHO They will try to close the door on you, just open it.
+IF /I "%var%" EQU "7" ECHO The key to more success is coco butter.
+IF /I "%var%" EQU "9" ECHO They dont want you to jet ski.
+IF /I "%var%" EQU "10" ECHO Those that weather the storm are the great ones.
+IF /I "%var%" EQU "11" ECHO You smart! You loyal! Youre a genius!
+IF /I "%var%" EQU "12" ECHO Baby, you smart. I want you to film me taking a shower.
+IF /I "%var%" EQU "13" ECHO You want my advice? Dont play yourself.
+IF /I "%var%" EQU "14" ECHO Always have faith. Always have hope.
+IF /I "%var%" EQU "15" ECHO Key to more success is clean heart and clean face.
+IF /I "%var%" EQU "16" ECHO Smh they get mad when u have joy.
+IF /I "%var%" EQU "17" ECHO I dont have no favorite rock bands. Im a fan of rock music though.
+IF /I "%var%" EQU "18" ECHO I wanted to see what type of trees is growing in Marcy Projects, && echo what type of water Jay Z was drinking.
+IF /I "%var%" EQU "19" ECHO Those that weather the storm r the great ones.
+IF /I "%var%" EQU "20" ECHO The other day the grass was brown, its green cuz i aint give up. && echo Never surrender.
+IF /I "%var%" EQU "21" ECHO Give thanks to the most high.
+IF /I "%var%" EQU "22" ECHO They dont want you to win. They dont want you to have the No. 1 record in the country. && echo They dont want you get healthy. They dont want you to exercise. && echo And they dont want you to have that view.
+IF /I "%var%" EQU "23" ECHO We go hard. In everything we do we’re going to accomplish our victory and our goal. If it takes a day, a year, or 20 years, we’re going to win.
+IF /I "%var%" EQU "24"
+IF /I "%var%" EQU "25"
+IF /I "%var%" EQU "26"
+IF /I "%var%" EQU "27"
+IF /I "%var%" EQU "28"
+IF /I "%var%" EQU "29"
+IF /I "%var%" EQU "30"
+IF /I "%var%" EQU "31"
+IF /I "%var%" EQU "32"
+IF /I "%var%" EQU "33"
+
+
+ECHO.
+ECHO - DJ Khaled
+ECHO.
+
+ECHO ______________________________________________________________________________________________
+ECHO.
+ECHO Anotha One? (y/n)
+ECHO.
+ECHO.
 
 pause
